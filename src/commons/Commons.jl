366fda83508b8f2d4dc35cf1d5e690effd123d64
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2025-12-18 19:35:49 +0100

    feat(commons): export and implement domain fallback

--- src/commons/Commons.jl
+++ src/commons/Commons.jl
@@ -4,11 +4,18 @@ include("docstringextension.jl")
 include("consts.jl")
 
 
-export get_description, add!
+export get_description, add!, domain
 
 function get_description end
 
 function add! end
 
+"""
+Return the domain tag type for objects that carry one.
+
+Fallback returns `nothing` for domainless objects.
+"""
+@inline domain(::Type) = nothing
+@inline domain(x) = domain(typeof(x))
 
 end
