145dcccd8f25741e5a11e6959d575bce2f95acd8
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2025-12-21 19:58:23 +0100

    feat(uq): add complex quantity support and as keyword to MCStatsPlotSpec

--- src/uq/plotspecs/mcstatsplotspec.jl
+++ src/uq/plotspecs/mcstatsplotspec.jl
@@ -39,6 +39,7 @@ PlotBuilder.axis_quantity(::Type{MCStatsPlotSpec}, ::Val{:y}, ::Val{:G}) =
 PlotBuilder.data_container(::Type{MCStatsPlotSpec}, ::Val{:x}) = nothing         # obj.f
 PlotBuilder.data_container(::Type{MCStatsPlotSpec}, ::Val{:y}) = :stats          # obj.stats[Sym]
 
+PlotBuilder.has_complex_qty(::Type{MCStatsPlotSpec}, ::Val{:y}, ::Val{:R}) = true
 
 # Define plot title
 function PlotBuilder.default_title(::Type{MCStatsPlotSpec}, nt::NamedTuple)
@@ -67,7 +68,7 @@ end
 
 # Semantic knobs:
 #   :field  → which field from stats NamedTuple
-PlotBuilder.input_kwargs(::Type{MCStatsPlotSpec}) = (:field,)
+PlotBuilder.input_kwargs(::Type{MCStatsPlotSpec}) = (:field, :as)
 
 # Backend knobs this spec forwards to Makie
 PlotBuilder.renderer_kwargs(::Type{MCStatsPlotSpec}) = ()
