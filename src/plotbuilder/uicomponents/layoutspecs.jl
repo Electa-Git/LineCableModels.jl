52856f67c359e913e0813e9c4db6fc3ee3c61add
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2025-12-27 22:31:33 +0100

    refactor(uicomponents): refactor layout logic to use make_layout and update slot definitions

--- src/plotbuilder/uicomponents/layoutspecs.jl
+++ src/plotbuilder/uicomponents/layoutspecs.jl
@@ -1,34 +1,67 @@
-# Trait surface
-function layout_spec(::Type{S}, ::Val{layout}) where {S, layout}
-	return layout_spec(Val(layout))
-end
-
 # -------------------------
-# Layout-only fallbacks
+# The Architect: make_layout
 # -------------------------
 
-# :windows -> toolbar / canvas / status (single view)
-function layout_spec(::Val{:windows})
-	containers = UIContainerSpec[]
-	slots = UISlotSpec[
-		UISlotSpec(:toolbar, :root, (1, 1); props = (; rowsize = Makie.Auto(0))),
-		UISlotSpec(:canvas, :root, (2, 1); props = (; rowsize = Makie.Auto())),
-		UISlotSpec(:status, :root, (3, 1); props = (; rowsize = Makie.Auto(0))),
-	]
-	return UILayoutSpec(:windows, containers, slots)
+function make_layout(::Val{layout}) where {layout}
+	error("make_layout(Val(:$layout)) not implemented")
 end
 
-function layout_spec(::Val{:grid})
-	containers = UIContainerSpec[]
-	slots = UISlotSpec[
-		UISlotSpec(:toolbar, :root, (1, 1); props = (; rowsize = Makie.Auto(0))),
-		UISlotSpec(:panelgrid, :root, (2, 1); props = (; rowsize = Makie.Auto())),
-		UISlotSpec(:status, :root, (3, 1); props = (; rowsize = Makie.Auto(0))),
+const _LEGEND_WIDTH = 150
+const _TOOLBAR_H = 36
+const _STATUS_H = 20
+
+function make_layout(::Val{:windows})
+	# 3x2 Grid
+
+	slots = [
+		# Toolbar
+		# layout: Container behavior (height, padding)
+		UISlotSpec(:toolbar, :root, (1, 1);
+			layout = (;
+				height = _TOOLBAR_H,
+				tellheight = true,
+				alignmode = Makie.Outside(5),
+			),
+		),
+
+		# Canvas
+		# layout: Standard plot area
+		UISlotSpec(:canvas, :root, (2, 1);
+			layout = (; alignmode = Makie.Inside()),
+		),
+
+		# Status
+		UISlotSpec(:status, :root, (3, 1);
+			layout = (;
+				height = _STATUS_H,
+				tellheight = true,
+				alignmode = Makie.Outside(2),
+			),
+		),
+
+		# Legend
+		# layout: Grid behavior (padding from edge)
+		# attrs:  Legend behavior (anchor to top)
+		UISlotSpec(:legend, :root, (1:3, 2);
+			layout = (; alignmode = Makie.Outside(5)),
+			attrs  = (; valign = :top),
+		),
 	]
-	return UILayoutSpec(:grid, containers, slots)
+
+	rs = Dict(
+		:root => Any[
+			Makie.Fixed(_TOOLBAR_H),
+			Makie.Relative(1.0),
+			Makie.Fixed(_STATUS_H),
+		],
+	)
+
+	cs = Dict(:root => Any[
+		Makie.Relative(1.0),
+		Makie.Fixed(_LEGEND_WIDTH),
+	])
+
+	return UILayoutSpec(:windows, UIContainerSpec[], slots, rs, cs)
 end
 
-# Last resort
-function layout_spec(::Val{layout}) where {layout}
-	error("layout_spec(Val($(layout))) not implemented")
-end
+make_layout(::Val{:grid}) = make_layout(Val(:windows))
