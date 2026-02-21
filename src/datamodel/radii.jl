125065fe0f3a73c3bdfb49d76e6d641c6f656da1
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2026-02-21 18:34:41 +0100

    style(datamodel): reformat radii parsing and normalize function signatures

--- src/datamodel/radii.jl
+++ src/datamodel/radii.jl
@@ -20,7 +20,7 @@ This function serves as a high-level interface to the radius resolution system. 
 - `radius_in`: Normalized inner radius \\[m\\].
 - `radius_ext`: Normalized outer radius \\[m\\].
 - `thickness`: Computed thickness or specialized dimension depending on the method \\[m\\].
-  For [`WireArray`](@ref) components, this value represents the wire radius instead of thickness.
+  For [`CircStrands`](@ref) components, this value represents the wire radius instead of thickness.
 
 # See also
 
@@ -29,7 +29,7 @@ This function serves as a high-level interface to the radius resolution system. 
 - [`AbstractCablePart`](@ref)
 """
 @inline _normalize_radii(::Type{T}, rin, rex) where {T} =
-  _do_normalize_radii(_parse_radius_operand(rin, T), _parse_radius_operand(rex, T), T)
+	_do_normalize_radii(_parse_radius_operand(rin, T), _parse_radius_operand(rex, T), T)
 
 """
 $(TYPEDSIGNATURES)
@@ -69,36 +69,60 @@ function _parse_radius_operand end
 @inline _parse_radius_operand(d::Diameter, ::Type{T}) where {T} = d.value / 2
 @inline _parse_radius_operand(p::Thickness, ::Type{T}) where {T} = p
 @inline function _parse_radius_operand(p::AbstractCablePart, ::Type{T}) where {T}
-  r = getfield(p, :radius_ext)                     # outer radius of prior layer
-  return (typeof(p) == T) ? r : to_certain(r)
+	r = getproperty(p, :radius_ext)                     # outer radius of prior layer
+	return (typeof(p) == T) ? r : to_certain(r)
 end
 @inline _parse_radius_operand(x::AbstractString, ::Type{T}) where {T} =
-  throw(ArgumentError("[$(nameof(T))] radius parameter must be numeric, not String: $(repr(x))"))
+	throw(
+		ArgumentError(
+			"[$(nameof(T))] radius parameter must be numeric, not String: $(repr(x))",
+		),
+	)
 @inline _parse_radius_operand(x, ::Type{T}) where {T} =
-  throw(ArgumentError("[$(nameof(T))] unsupported radius parameter $(typeof(x)): $(repr(x))"))
-
-
+	throw(
+		ArgumentError(
+			"[$(nameof(T))] unsupported radius parameter $(typeof(x)): $(repr(x))",
+		),
+	)
 
 # ------------ Input parsing
-@inline function _do_normalize_radii(radius_in::Number, radius_ext::Number, ::Type{T}) where {T}
-  return radius_in, radius_ext
+@inline function _do_normalize_radii(
+	radius_in::Number,
+	radius_ext::Number,
+	::Type{T},
+) where {T}
+	return radius_in, radius_ext
 end
 
-@inline function _do_normalize_radii(radius_in::Number, thickness::Thickness, ::Type{T}) where {T}
-  return radius_in, (radius_in + thickness.value)
+@inline function _do_normalize_radii(
+	radius_in::Number,
+	thickness::Thickness,
+	::Type{T},
+) where {T}
+	return radius_in, (radius_in + thickness.value)
 end
 
-@inline function _do_normalize_radii(radius_in::Number, radius_wire::Number, ::Type{AbstractWireArray})
-  return radius_in, radius_in + (2 * radius_wire)
+@inline function _do_normalize_radii(
+	radius_in::Number,
+	radius_wire::Number,
+	::Type{AbstractStrandsLayer},
+)
+	return radius_in, radius_in + (2 * radius_wire)
 end
 
 @inline function _do_normalize_radii(t::Thickness, rex::Number, ::Type{T}) where {T}
-  rin = rex - t.value
-  rin >= 0 || throw(ArgumentError("[$(nameof(T))] thickness $(t.value) exceeds outer radius $(rex)."))
-  return rin, rex
+	rin = rex - t.value
+	rin >= 0 || throw(
+		ArgumentError("[$(nameof(T))] thickness $(t.value) exceeds outer radius $(rex)."),
+	)
+	return rin, rex
 end
 
 # NEW: reject thickness on BOTH ends
 @inline function _do_normalize_radii(::Thickness, ::Thickness, ::Type{T}) where {T}
-  throw(ArgumentError("[$(nameof(T))] cannot specify thickness for both inner and outer radii."))
+	throw(
+		ArgumentError(
+			"[$(nameof(T))] cannot specify thickness for both inner and outer radii.",
+		),
+	)
 end
