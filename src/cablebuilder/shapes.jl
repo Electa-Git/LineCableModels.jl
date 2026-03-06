b34dcf726308c7fe95aa8c0e523c56a1a8b684a9
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2026-03-06 22:11:39 +0100

    feat(shapes): add global accessors and include circstranded shape

--- src/cablebuilder/shapes.jl
+++ src/cablebuilder/shapes.jl
@@ -8,6 +8,12 @@ abstract type AbstractShape{L <: AbstractLayout, T <: Real} end
 import .Validation:
 	Validation, required_fields, extra_rules, Normalized, Finite, Nonneg, Less, Positive
 
+
+# Global accessors
+@inline r_in(s::AbstractShape) = s.r_in
+@inline r_ex(s::AbstractShape) = s.r_ex
+
 include("solidcore.jl")
 include("tubular.jl")
 include("enclosure.jl")
+include("circstranded.jl")
