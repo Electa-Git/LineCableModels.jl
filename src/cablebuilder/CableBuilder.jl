4f5f5ce916596e006f7d342b0ee14795c892bd5d
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2026-03-06 22:11:00 +0100

    refactor(cablebuilder): update exports and rename Conductor/Insulator APIs

--- src/cablebuilder/CableBuilder.jl
+++ src/cablebuilder/CableBuilder.jl
@@ -17,45 +17,59 @@ include("types.jl")
 include("cabledesign.jl")
 
 
-export Material,
-	MaterialSpec, build, Conductor, AbstractCablePart, CableDesign, CableDesignSpec
-export SolidCore, TubularShape, Concentric, ConductorPart, Enclosure
+export Material, MaterialSpec, AbstractCablePart, CableDesign, CableDesignSpec, build
+export Concentric, SectorShaped
+export ConductorPart, InsulatorPart
+export SolidCore, TubularShape, CircStrandedShape, Enclosure
 export Grid, DeterministicGrid, RelativeGrid, AbsoluteGrid, AbsoluteError, AbstractSpec,
-	EnclosureSpec
+	EnclosureSpec, TubularLayerSpec, SolidCoreSpec, CircStrandedSpec
 
 # ==========================================
 # THE FRONTEND API (Compilation boundary)
 # ==========================================
-export SolidCore, TubularShape, Concentric, ConductorPart
-
+export Conductor, Insulator
 module Conductor
-	import ..CableBuilder: SolidCoreSpec, TubularPartSpec, ConductorPart, Grid
-	import ..CableBuilder: AbstractSpec, Material, EnclosureSpec
+	import ..CableBuilder: SolidCoreSpec, TubularLayerSpec, ConductorPart, Grid
+	import ..CableBuilder: AbstractSpec, Material, EnclosureSpec, CircStrandedSpec
 
-	@inline function Solid(tag::Symbol, mat; r)
+	@inline function SolidCore(cmp::Symbol, mat; r)
 		mat_spec = convert(AbstractSpec{Material}, mat)
-		return SolidCoreSpec(ConductorPart, Grid(tag), Grid(r), mat_spec)
+		return SolidCoreSpec(ConductorPart, Grid(cmp), Grid(r), mat_spec)
 	end
 
-	@inline function Tubular(tag::Symbol, mat; t)
+	@inline function Tubular(cmp::Symbol, mat; t)
 		mat_spec = convert(AbstractSpec{Material}, mat)
-		return TubularPartSpec(ConductorPart, Grid(tag), Grid(t), mat_spec)
+		return TubularLayerSpec(ConductorPart, Grid(cmp), Grid(t), mat_spec)
 	end
 
-	@inline function Pipe(tag::Symbol, mat; t, filler, offset = 0.0)
-		inner = Tubular(tag, mat; t = t)          # tubular wall
+	@inline function Pipe(cmp::Symbol, mat; t, filler, offset = 0.0)
+		inner = Tubular(cmp, mat; t = t)          # tubular wall
 		return EnclosureSpec(ConductorPart, inner, filler; offset = offset)
 	end
+
+	@inline function Wires(cmp::Symbol, mat; r_w, n_w, lay_r, lay_d = 1)
+		mat_spec = convert(AbstractSpec{Material}, mat)
+		return CircStrandedSpec(
+			ConductorPart,
+			Grid(cmp),
+			Grid(r_w),
+			Grid(n_w),
+			Grid(lay_r),
+			Grid(lay_d),
+			mat_spec,
+		)
+	end
+
 end
 
 module Insulator
-	import ..CableBuilder: SolidCoreSpec, TubularPartSpec, InsulatorPart, Grid
+	import ..CableBuilder: SolidCoreSpec, TubularLayerSpec, InsulatorPart, Grid
 	import ..CableBuilder: AbstractSpec, Material, EnclosureSpec
 
 	# Insulators don't usually have solid cores, but the logic holds!
-	@inline function Tubular(tag::Symbol, mat; t)
+	@inline function Tubular(cmp::Symbol, mat; t)
 		mat_spec = convert(AbstractSpec{Material}, mat)
-		return TubularPartSpec(InsulatorPart, Grid(tag), Grid(t), mat_spec)
+		return TubularLayerSpec(InsulatorPart, Grid(cmp), Grid(t), mat_spec)
 	end
 end
 
