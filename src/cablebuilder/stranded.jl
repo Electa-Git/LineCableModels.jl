c8b7709b54d543e8d9af38989235f6bcd0a5b615
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2026-03-12 14:14:31 +0100

    refactor(stranded): unify CircStranded types into StrandedLayer, StrandedBuilder, and StrandedSpec

--- src/cablebuilder/stranded.jl
+++ src/cablebuilder/stranded.jl
@@ -1,81 +1,93 @@
-# The Vault
-struct CircStrandedShape{L, T <: Real, U <: Integer} <: AbstractShape{L, T}
+# ==========================================
+# 1. THE UNIVERSAL VAULT
+# ==========================================
+struct StrandedLayer{
+	L,
+	T <: Real,
+	U <: Integer,
+	P <: AbstractWire{T},
+	H <: HelicalPath,
+} <: AbstractShape{L, T}
 	r_in::T
 	r_ex::T
-	r_w::T
 	n_w::U
-	lay_r::T
-	lay_d::U
+	wire::P
+	pitch::H
 end
 
-# The Janitor
-function CircStrandedShape{L}(
-	r_in, r_ex, r_w, n_w::U, lay_r, lay_d::U,
-) where {L, U <: Integer}
-	T = promote_type(typeof(r_in), typeof(r_ex), typeof(r_w), typeof(lay_r))
-	return CircStrandedShape{L, T, U}(
-		convert(T, r_in),
-		convert(T, r_ex),
-		convert(T, r_w),
-		n_w,
-		convert(T, lay_r),
-		lay_d,
+function StrandedLayer{L}(
+	r_in,
+	r_ex,
+	n_w::Integer,
+	wire::AbstractWire,
+	pitch::HelicalPath,
+) where {L}
+	T = promote_type(typeof(r_in), typeof(r_ex))
+	p = convert(AbstractWire{T}, wire)
+	return StrandedLayer{L, T, typeof(n_w), typeof(p), typeof(pitch)}(
+		convert(T, r_in), convert(T, r_ex), n_w, p, pitch,
 	)
 end
 
-# The Diplomat
 function Base.convert(
 	::Type{<:AbstractShape{L, T}},
-	s::CircStrandedShape{L},
+	s::StrandedLayer{L},
 ) where {L, T <: Real}
-	return CircStrandedShape{L, T, typeof(s.n_w)}(
-		convert(T, s.r_in),
-		convert(T, s.r_ex),
-		convert(T, s.r_w),
-		s.n_w,
-		convert(T, s.lay_r),
-		s.lay_d,
+	p_converted = convert(AbstractWire{T}, s.wire)
+	return StrandedLayer{L, T, typeof(s.n_w), typeof(p_converted), typeof(s.pitch)}(
+		convert(T, s.r_in), convert(T, s.r_ex), s.n_w, p_converted, s.pitch,
 	)
 end
 
-struct CircStrandedBuilder{P, Tgeom <: Real, Tmat <: Real, U <: Integer}
-	cmp::Symbol
-	r_w::Tgeom
+# ==========================================
+# 2. THE UNIVERSAL BUILDER
+# ==========================================
+struct StrandedBuilder{P, U <: Integer, W, H, T <: Real}
+	grp::Symbol
 	n_w::U
-	lay_r::Tgeom
-	lay_d::U
-	mat::Material{Tmat}
+	wire_builder::W
+	pitch_builder::H
+	mat::Material{T}
 end
 
-@inline function CircStrandedBuilder{P}(
-	cmp::Symbol, r_w::Tgeom, n_w::U, lay_r::Tgeom, lay_d::U, mat::Material{Tmat},
-) where {P, Tgeom, Tmat, U <: Integer}
-	return CircStrandedBuilder{P, Tgeom, Tmat, U}(cmp, r_w, n_w, lay_r, lay_d, mat)
-end
-
-@inline function (b::CircStrandedBuilder{P})(current_r::T) where {P, T <: Real}
-	# Strict geometric continuation. r_ex is fully determined by current_r + diameter.
-	r_ex = current_r + 2 * b.r_w
-	shape = CircStrandedShape{Concentric}(current_r, r_ex, b.r_w, b.n_w, b.lay_r, b.lay_d)
-
-	# P is the ConductorPart. This call will trigger the T = promote_type(...) rule.
-	return P(b.cmp, shape, b.mat)
-end
-
-struct CircStrandedSpec{P, Tcmp, Trw, Tnw, Tlayr, Tlayd, M <: AbstractSpec{Material}} <:
-	   AbstractSpec{CircStrandedBuilder{P}}
-	cmp::Tcmp
-	r_w::Trw
-	n_w::Tnw
-	lay_r::Tlayr
-	lay_d::Tlayd
-	mat::M
-end
-
-@inline function CircStrandedSpec(
-	::Type{P}, cmp::Tcmp, r_w::Trw, n_w::Tnw, lay_r::Tlayr, lay_d::Tlayd, mat::M,
-) where {P, Tcmp, Trw, Tnw, Tlayr, Tlayd, M <: AbstractSpec{Material}}
-	return CircStrandedSpec{P, Tcmp, Trw, Tnw, Tlayr, Tlayd, M}(
-		cmp, r_w, n_w, lay_r, lay_d, mat,
+@inline function (b::StrandedBuilder{P})(current_r::T) where {P, T <: Real}
+	# If someone tries to put a stranded armor at the exact center of the universe, mock them.
+	current_r <= zero(T) && error(
+		"Topological violation: Stranded layers cannot exist at r=0. Use a SolidCore.",
 	)
+
+	# 1. Materialize the physical entity
+	wire = b.wire_builder()
+
+	# 2. Extract its radial footprint via dispatch
+	thick = char_len(wire)
+
+	r_ex = current_r + thick
+	mean_diam = current_r + (thick / 2)
+
+	# 3. Pass context to the nested helical builder
+	pitch_profile = b.pitch_builder(mean_diam)
+
+	# 4. Lock it into the unified layer
+	shape = StrandedLayer{Concentric}(current_r, r_ex, b.n_w, wire, pitch_profile)
+
+	return P(b.grp, shape, b.mat)
+end
+
+# ==========================================
+# 3. THE UNIVERSAL BLUEPRINT
+# ==========================================
+struct StrandedSpec{
+	P,
+	G,
+	U,
+	W <: AbstractSpec,
+	H <: AbstractSpec,
+	M <: AbstractSpec{Material},
+} <: AbstractSpec{StrandedBuilder{P}}
+	grp::G
+	n_w::U
+	wire_spec::W
+	pitch_spec::H
+	mat::M
 end
