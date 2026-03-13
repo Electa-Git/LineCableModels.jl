c692042e7992d2c14248eb90398d3faba18c6052
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2026-03-13 17:46:14 +0100

    refactor(shapes): comment out inline char_len methods for TubularLayer, CircularWire, and RectangularWire

--- src/cablebuilder/shapes.jl
+++ src/cablebuilder/shapes.jl
@@ -24,6 +24,6 @@ include("solidcore.jl")
 # If someone writes a Rectangle primitive and defines char_len as the diagonal, 
 # the stacking engine will blindly build overlapping garbage.
 @inline char_len(s::SolidCore) = 2 * r_ex(s)
-@inline char_len(s::TubularLayer) = r_ex(s) - r_in(s)
-@inline char_len(s::CircularWire) = 2 * s.r
-@inline char_len(s::RectangularWire) = s.h
+# @inline char_len(s::TubularLayer) = r_ex(s) - r_in(s)
+# @inline char_len(s::CircularWire) = 2 * s.r
+# @inline char_len(s::RectangularWire) = s.h
