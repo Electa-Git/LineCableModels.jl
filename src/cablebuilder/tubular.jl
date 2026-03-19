e88d432a4aba9901dffda94926e88752106149b3
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2026-03-19 14:53:45 +0100

    refactor(tubular): update TubularLayer struct and constructor logic

--- src/cablebuilder/tubular.jl
+++ src/cablebuilder/tubular.jl
@@ -1,21 +1,21 @@
 # ==========================================
 # 1. THE VAULT
 # ==========================================
-struct TubularLayer{L, T <: Real} <: AbstractShape{L, T}
+struct TubularLayer{T <: Real} <: AbstractShape{T}
 	r_in::T
 	r_ex::T
 end
 
-function TubularLayer{L}(r_in, r_ex) where {L}
+function TubularLayer(r_in, r_ex)
 	T = promote_type(typeof(r_in), typeof(r_ex))
-	return TubularLayer{L, T}(convert(T, r_in), convert(T, r_ex))
+	return TubularLayer{T}(convert(T, r_in), convert(T, r_ex))
 end
 
 function Base.convert(
-	::Type{<:AbstractShape{L, T}},
-	s::TubularLayer{L},
-) where {L, T <: Real}
-	return TubularLayer{L, T}(convert(T, s.r_in), convert(T, s.r_ex))
+	::Type{<:AbstractShape{T}},
+	s::TubularLayer,
+) where {T <: Real}
+	return TubularLayer{T}(convert(T, s.r_in), convert(T, s.r_ex))
 end
 
 # ==========================================
@@ -39,7 +39,7 @@ end
 @inline function (b::TubularLayerBuilder{P})(current_r::T) where {P, T <: Real}
 	# Physics reminder: r_ex = r_in + thickness. 
 	r_ex = current_r + b.t
-	shape = TubularLayer{Concentric}(current_r, r_ex)
+	shape = TubularLayer(current_r, r_ex)
 	return P(b.grp, shape, b.mat)
 end
 
