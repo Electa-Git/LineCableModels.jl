dfab4677e70bdf21b5b62c72d8e6fb7eacb4ddb8
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2026-03-19 14:53:58 +0100

    feat(primitives): add primitives.jl file

---
+++ src/cablebuilder/primitives.jl
@@ -0,0 +1,33 @@
+# Shape params define the shape of a primitive, but not its material, group or location/layout.
+abstract type AbstractShapeParams{T <: Real} end
+
+@gridspace @relax struct Circular{T <: Real} <: AbstractShapeParams{T}
+	r::T
+end
+# # Teach Base how to cast the abstract type down to this specific concrete type
+# @inline Base.convert(::Type{AbstractShapeParams{T}}, p::Circular) where {T <: Real} =
+# 	convert(Circular{T}, p)
+
+@inline function validate(p::Circular)
+	p.r > zero(p.r) || throw(DomainError(p.r, "Circular radius must be strictly positive."))
+	return p
+end
+
+@gridspace @relax struct Rectangular{T <: Real} <: AbstractShapeParams{T}
+	w::T
+	h::T
+end
+# # Teach Base how to cast the abstract type down to this specific concrete type
+# @inline Base.convert(::Type{AbstractShapeParams{T}}, p::Rectangular) where {T <: Real} =
+# 	convert(Rectangular{T}, p)
+
+@inline function validate(p::Rectangular)
+	p.w > zero(p.w) ||
+		throw(DomainError(p.w, "Rectangular width must be strictly positive."))
+	p.h > zero(p.h) ||
+		throw(DomainError(p.h, "Rectangular height must be strictly positive."))
+	return p
+end
+
+# If a specific payload vault doesn't define intrinsic rules, it passes.
+@inline validate(params::AbstractShapeParams) = params
