7e39b1420e42fb838e4b912a62a8b04e19b94ff6
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2026-03-20 15:15:42 +0100

    refactor(types): add origin field to ConductorPart and InsulatorPart and update constructors

--- src/cablebuilder/types.jl
+++ src/cablebuilder/types.jl
@@ -6,44 +6,50 @@ abstract type AbstractCablePart end
 
 struct ConductorPart{T, S <: AbstractShape{T}} <: AbstractCablePart
 	grp::Symbol
+	origin::Tuple{T, T}
 	shape::S
 	material::Material{T}
 end
 
 @inline function ConductorPart(
 	grp::Symbol,
+	origin::Tuple{O1, O2},
 	shape::AbstractShape{S},
 	mat::Material{M},
-) where {S <: Real, M <: Real}
-	# 1. Ask the compiler what the safest common type is.
-	T = promote_type(S, M)
+) where {O1 <: Real, O2 <: Real, S <: Real, M <: Real}
+	# Ask the compiler what the safest common type is.
+	T = promote_type(O1, O2, S, M)
 
-	# 2. Let native dispatch handle the translation.
+	# Let native dispatch handle the translation.
+	o = (convert(T, origin[1]), convert(T, origin[2]))
 	s = convert(AbstractShape{T}, shape)
 	m = convert(Material{T}, mat)
 
-	return ConductorPart{T, typeof(s)}(grp, s, m)
+	return ConductorPart{T, typeof(s)}(grp, o, s, m)
 end
 
 struct InsulatorPart{T, S <: AbstractShape{T}} <: AbstractCablePart
 	grp::Symbol
+	origin::Tuple{T, T}
 	shape::S
 	material::Material{T}
 end
 
 @inline function InsulatorPart(
 	grp::Symbol,
+	origin::Tuple{O1, O2},
 	shape::AbstractShape{S},
 	mat::Material{M},
-) where {S <: Real, M <: Real}
-	# 1. Ask the compiler what the safest common type is.
-	T = promote_type(S, M)
+) where {O1 <: Real, O2 <: Real, S <: Real, M <: Real}
+	# Ask the compiler what the safest common type is.
+	T = promote_type(O1, O2, S, M)
 
-	# 2. Let native dispatch handle the translation.
+	# Let native dispatch handle the translation.
+	o = (convert(T, origin[1]), convert(T, origin[2]))
 	s = convert(AbstractShape{T}, shape)
 	m = convert(Material{T}, mat)
 
-	return InsulatorPart{T, typeof(s)}(grp, s, m)
+	return InsulatorPart{T, typeof(s)}(grp, o, s, m)
 end
 
 # ---------------------------------------------------------
