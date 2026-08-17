475d953f5c3cc57e0ee76d3a2374f24ba06189d3
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2026-08-17 04:05:03 +0200

    feat(datamodel):  unslopify pscad export method

--- src/importexport/pscad/schema.jl
+++ src/importexport/pscad/schema.jl
@@ -9,17 +9,27 @@ function _pscad_attributes!(node::EzXML.Node, attributes)
     return node
 end
 
-function _pscad_params!(parent::EzXML.Node, parameters)
+function _pscad_paramlist!(
+        parent::EzXML.Node,
+        parameters;
+        name = nothing,
+        link = nothing,
+        crc = nothing
+)
     list = addelement!(parent, "paramlist")
-    list["name"] = ""
-    list["link"] = "-1"
-    list["crc"] = "-1"
+    name === nothing || (list["name"] = string(name))
+    link === nothing || (list["link"] = string(link))
+    crc === nothing || (list["crc"] = string(crc))
     for (name, value) in parameters
         parameter = addelement!(list, "param")
         parameter["name"] = string(name)
         parameter["value"] = string(value)
     end
     return list
+end
+
+function _pscad_params!(parent::EzXML.Node, parameters)
+    return _pscad_paramlist!(parent, parameters; name = "", link = -1, crc = -1)
 end
 
 function _pscad_user!(
@@ -49,6 +59,70 @@ function _pscad_user!(
         ))
     _pscad_params!(user, parameters)
     return user
+end
+
+function _pscad_wire!(
+        parent::EzXML.Node,
+        wire_id::Integer,
+        user_id::Integer,
+        kind::AbstractString,
+        name::AbstractString,
+        definition::AbstractString,
+        user_binding::AbstractString,
+        parameters;
+        x::Integer,
+        y::Integer,
+        width::Integer,
+        height::Integer,
+        crc = nothing
+)
+    wire = addelement!(parent, "Wire")
+    attributes = (
+        id = wire_id,
+        name,
+        classid = kind,
+        x,
+        y,
+        w = width,
+        h = height,
+        orient = 0,
+        defn = definition,
+        recv = -1,
+        send = -1,
+        back = -1,
+        disable = "false"
+    )
+    _pscad_attributes!(wire, attributes)
+    crc === nothing || (wire["crc"] = string(crc))
+    for (vertex_x, vertex_y) in ((0, 0), (0, 18), (54, 54), (54, 72))
+        vertex = addelement!(wire, "vertex")
+        _pscad_attributes!(vertex, (x = vertex_x, y = vertex_y))
+    end
+    _pscad_user!(wire, user_id, user_binding, parameters)
+    return wire
+end
+
+function _pscad_canvas_params!(canvas::EzXML.Node; user::Bool = false)
+    parameters = Pair{String, String}[
+        "show_grid" => "0",
+        "size" => "0",
+        "orient" => "1",
+        "show_border" => "0",
+        "monitor_bus_voltage" => "0",
+        "show_signal" => "0",
+        "show_virtual" => "0",
+        "show_sequence" => "0",
+        "auto_sequence" => "1",
+        "bus_expand_x" => "8",
+        "bus_expand_y" => "8",
+        "bus_length" => "4"
+    ]
+    user && append!(parameters, [
+        "show_terminals" => "0",
+        "virtual_filter" => "",
+        "animation_freq" => "500"
+    ])
+    return _pscad_paramlist!(canvas, parameters)
 end
 
 function _pscad_value(value; sigdigits::Integer = 6, minimum = -Inf, maximum = Inf)
--- test/integration/exporters.jl
+++ test/integration/exporters.jl
@@ -25,7 +25,17 @@
         @test nodename(project) == "project"
         @test project["name"] == system.system_id
 
-        components=findall("//User", document)
+        definitions=findall("./definitions/Definition", project)
+        @test length(definitions) == 3
+        station=only(filter(node->node["name"]=="DS", definitions))
+        main=only(filter(node->node["name"]=="Main", definitions))
+        cable_definition=only(filter(
+            node->node["name"]=="CableSystem", definitions))
+        @test station["classid"] == "StationDefn"
+        @test main["classid"] == "UserCmpDefn"
+        @test cable_definition["classid"] == "RowDefn"
+
+        components=findall("./schematic/User", cable_definition)
         @test length(components) == length(system.cables) + 2
         @test getindex.(components, "id") ==
               string.(100000010:(100000011 + length(system.cables)))
@@ -63,10 +73,37 @@
         @test ground_values["GPERM"] == "1.0"
         @test ground_values["GRP"] == "10.0"
 
-        definition=only(findall("//Definition", document))
-        @test definition["name"] == "CableSystem"
-        @test only(findall("./schematic", definition))["classid"] == "RowCanvas"
-        @test isempty(findall("//Wire", document))
+        @test only(findall("./schematic", cable_definition))["classid"] == "RowCanvas"
+
+        station_wire=only(findall("./schematic/Wire", station))
+        @test station_wire["classid"] == "Branch"
+        @test station_wire["name"] == "Main"
+        @test station_wire["defn"] == "Main"
+        main_reference=only(findall("./User", station_wire))
+        @test main_reference["name"] == "$(system.system_id):Main"
+        @test main_reference["defn"] == "$(system.system_id):Main"
+
+        main_canvas=only(findall("./schematic", main))
+        @test main_canvas["classid"] == "UserCanvas"
+        cable_wire=only(findall("./Wire", main_canvas))
+        @test cable_wire["classid"] == "Cable"
+        @test cable_wire["name"] == "$(system.system_id):CableSystem"
+        cable_reference=only(findall("./User", cable_wire))
+        @test cable_reference["defn"] == "$(system.system_id):CableSystem"
+        instance_values=parameters(cable_reference)
+        @test instance_values["Name"] == system.system_id
+        @test parse(Float64, instance_values["Freq"]) == 50.0
+        @test parse(Float64, instance_values["Length"]) == system.line_length / 1000
+
+        hierarchy=only(findall("./hierarchy/call", project))
+        @test hierarchy["link"] == "100000001"
+        @test hierarchy["name"] == "$(system.system_id):DS"
+        main_call=only(findall("./call", hierarchy))
+        @test main_call["link"] == "100000003"
+        cable_call=only(findall("./call", main_call))
+        @test cable_call["link"] == "100000008"
+        @test cable_call["view"] == "true"
+        @test length(findall("//Wire", document)) == 2
         @test occursin("LineCableModels.jl", read(output, String))
     end
 end
